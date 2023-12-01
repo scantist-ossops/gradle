812fea5412986ba7bf6b912d2cd2c17233a896ac
Anže Sodja <asodja@gradle.com>
2023-12-02 00:01:43 +0100

    Another hack

--- subprojects/core/src/main/java/org/gradle/api/internal/initialization/transform/BaseInstrumentingArtifactTransform.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/initialization/transform/BaseInstrumentingArtifactTransform.java
@@ -16,40 +16,23 @@
 
 package org.gradle.api.internal.initialization.transform;
 
-import org.gradle.api.artifacts.transform.InputArtifact;
 import org.gradle.api.artifacts.transform.TransformAction;
-import org.gradle.api.artifacts.transform.TransformOutputs;
 import org.gradle.api.artifacts.transform.TransformParameters;
-import org.gradle.api.file.FileSystemLocation;
-import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
-import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.PathSensitive;
-import org.gradle.api.tasks.PathSensitivity;
 import org.gradle.cache.GlobalCacheLocations;
 import org.gradle.internal.classpath.ClasspathWalker;
 import org.gradle.internal.classpath.InPlaceClasspathBuilder;
-import org.gradle.internal.classpath.transforms.ClasspathElementTransform;
 import org.gradle.internal.classpath.transforms.ClasspathElementTransformFactory;
 import org.gradle.internal.classpath.transforms.ClasspathElementTransformFactoryForAgent;
 import org.gradle.internal.classpath.transforms.ClasspathElementTransformFactoryForLegacy;
-import org.gradle.internal.classpath.transforms.InstrumentingClassTransform;
-import org.gradle.internal.classpath.types.InstrumentingTypeRegistry;
 import org.gradle.internal.file.Stat;
-import org.gradle.util.internal.GFileUtils;
 import org.gradle.work.DisableCachingByDefault;
 
 import javax.inject.Inject;
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
 
 import static org.gradle.api.internal.initialization.transform.BaseInstrumentingArtifactTransform.InstrumentArtifactTransformParameters;
-import static org.gradle.internal.classpath.TransformedClassPath.INSTRUMENTED_JAR_DIR_NAME;
-import static org.gradle.internal.classpath.TransformedClassPath.INSTRUMENTED_MARKER_FILE_NAME;
-import static org.gradle.internal.classpath.TransformedClassPath.ORIGINAL_JAR_DIR_NAME;
 
 /**
  * Base artifact transform that instruments plugins with Gradle instrumentation, e.g. for configuration cache detection or property upgrades.
@@ -67,50 +50,6 @@ public abstract class BaseInstrumentingArtifactTransform implements TransformAct
         @Input
         @Optional
         Property<String> getUpgradedPropertiesHash();
-    }
-
-    @Inject
-    public abstract ObjectFactory getObjects();
-
-    @PathSensitive(PathSensitivity.NAME_ONLY)
-    @InputArtifact
-    public abstract Provider<FileSystemLocation> getInput();
-
-    @Override
-    public void transform(TransformOutputs outputs) {
-        File input = getInput().get().getAsFile();
-        if (!input.exists()) {
-            // Don't instrument files that don't exist, these could be files added to classpath via files()
-            return;
-        }
-
-        // A marker file that indicates that the result is instrumented jar,
-        // this is important so TransformedClassPath can correctly filter instrumented jars.
-        createNewFile(outputs.file(INSTRUMENTED_MARKER_FILE_NAME));
-
-        // Instrument jars
-        InstrumentationServices instrumentationServices = getObjects().newInstance(InstrumentationServices.class);
-        File outputFile = outputs.file(INSTRUMENTED_JAR_DIR_NAME + "/" + input.getName());
-        ClasspathElementTransformFactory transformFactory = instrumentationServices.getTransformFactory(getParameters().getAgentSupported().get());
-        ClasspathElementTransform transform = transformFactory.createTransformer(input, new InstrumentingClassTransform(), InstrumentingTypeRegistry.EMPTY);
-        transform.transform(outputFile);
-
-        // Copy original jars after in case they are not in global cache
-        if (instrumentationServices.getGlobalCacheLocations().isInsideGlobalCache(input.getAbsolutePath())) {
-            // The global caches are additive only, so we can use it directly since it shouldn't be deleted or changed during the build.
-            outputs.file(getInput());
-        } else {
-            File copyOfOriginalFile = outputs.file(ORIGINAL_JAR_DIR_NAME + "/" + input.getName());
-            GFileUtils.copyFile(input, copyOfOriginalFile);
-        }
-    }
-
-    private boolean createNewFile(File file) {
-        try {
-            return file.createNewFile();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
     }
 
 
--- subprojects/core/src/main/java/org/gradle/api/internal/initialization/transform/ExternalDependencyInstrumentingArtifactTransform.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/initialization/transform/ExternalDependencyInstrumentingArtifactTransform.java
@@ -16,12 +16,76 @@
 
 package org.gradle.api.internal.initialization.transform;
 
+import org.gradle.api.artifacts.transform.InputArtifact;
 import org.gradle.api.artifacts.transform.TransformAction;
+import org.gradle.api.artifacts.transform.TransformOutputs;
+import org.gradle.api.file.FileSystemLocation;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.internal.classpath.transforms.ClasspathElementTransform;
+import org.gradle.internal.classpath.transforms.ClasspathElementTransformFactory;
+import org.gradle.internal.classpath.transforms.InstrumentingClassTransform;
+import org.gradle.internal.classpath.types.InstrumentingTypeRegistry;
+import org.gradle.util.internal.GFileUtils;
 import org.gradle.work.DisableCachingByDefault;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+import static org.gradle.internal.classpath.TransformedClassPath.INSTRUMENTED_JAR_DIR_NAME;
+import static org.gradle.internal.classpath.TransformedClassPath.INSTRUMENTED_MARKER_FILE_NAME;
+import static org.gradle.internal.classpath.TransformedClassPath.ORIGINAL_JAR_DIR_NAME;
 
 /**
  * Artifact transform that instruments external plugins with Gradle instrumentation.
  */
 @DisableCachingByDefault(because = "Instrumented jars are too big to cache")
 public abstract class ExternalDependencyInstrumentingArtifactTransform extends BaseInstrumentingArtifactTransform implements TransformAction<BaseInstrumentingArtifactTransform.InstrumentArtifactTransformParameters> {
+    @Inject
+    public abstract ObjectFactory getObjects();
+
+    @PathSensitive(PathSensitivity.NAME_ONLY)
+    @InputArtifact
+    public abstract Provider<FileSystemLocation> getInput();
+
+    @Override
+    public void transform(TransformOutputs outputs) {
+        File input = getInput().get().getAsFile();
+        if (!input.exists()) {
+            // Don't instrument files that don't exist, these could be files added to classpath via files()
+            return;
+        }
+
+        // A marker file that indicates that the result is instrumented jar,
+        // this is important so TransformedClassPath can correctly filter instrumented jars.
+        createNewFile(outputs.file(INSTRUMENTED_MARKER_FILE_NAME));
+
+        // Instrument jars
+        InstrumentationServices instrumentationServices = getObjects().newInstance(InstrumentationServices.class);
+        File outputFile = outputs.file(INSTRUMENTED_JAR_DIR_NAME + "/" + input.getName());
+        ClasspathElementTransformFactory transformFactory = instrumentationServices.getTransformFactory(getParameters().getAgentSupported().get());
+        ClasspathElementTransform transform = transformFactory.createTransformer(input, new InstrumentingClassTransform(), InstrumentingTypeRegistry.EMPTY);
+        transform.transform(outputFile);
+
+        // Copy original jars after in case they are not in global cache
+        if (instrumentationServices.getGlobalCacheLocations().isInsideGlobalCache(input.getAbsolutePath())) {
+            // The global caches are additive only, so we can use it directly since it shouldn't be deleted or changed during the build.
+            outputs.file(getInput());
+        } else {
+            File copyOfOriginalFile = outputs.file(ORIGINAL_JAR_DIR_NAME + "/" + input.getName());
+            GFileUtils.copyFile(input, copyOfOriginalFile);
+        }
+    }
+
+    private boolean createNewFile(File file) {
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
